28505146bebf9db2ef602bcb747e001cb3cc4d0a
docty <=>
2019-08-08 13:00:42 +0000

    build

---
+++ client/build/precache-manifest.38e3896b12e4c00f55b2d5f28485cfa7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f2baead7d81a5a3c0b115c1a64937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f372c6977a7633234cb7",
+    "url": "/static/css/main.4089daa7.chunk.css"
+  },
+  {
+    "revision": "9a3e244bbd09df0b8d18",
+    "url": "/static/js/2.517adafb.chunk.js"
+  },
+  {
+    "revision": "f372c6977a7633234cb7",
+    "url": "/static/js/main.243982b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78122e06c6b70a47f5a1807e33fca660",
+    "url": "/static/media/laravel.78122e06.png"
+  },
+  {
+    "revision": "6d5a1a24c2858f99e2f8f66bee16eac2",
+    "url": "/static/media/mysql.6d5a1a24.png"
+  },
+  {
+    "revision": "574195b0ae7f4a5fb17abe3f29458d13",
+    "url": "/static/media/php.574195b0.png"
+  },
+  {
+    "revision": "9dc296dd8741e61cfce6026ce7816612",
+    "url": "/static/media/react.9dc296dd.png"
+  },
+  {
+    "revision": "2050199291950f22265091fd1bcb038e",
+    "url": "/static/media/unity3d.20501992.png"
+  }
+]);
