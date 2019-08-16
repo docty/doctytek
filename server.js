18f4a5d0fc071369cf2b865a1b7171367787e66a
docty <=>
2019-08-16 14:46:24 +0000

    reversing

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-import express from 'express';
-import path from 'path';
-import mysql from 'mysql2';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-require('dotenv').config();
-
-
-import Tasks from '../app/controller/task';
-import Feedbacks from '../app/controller/feedback';
+const  express = require('express');
+//import express from 'express';
+// import path from 'path';
+// import mysql from 'mysql2';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+ require('dotenv').config();
+//
+//
+// import Tasks from '../app/controller/task';
+// import Feedbacks from '../app/controller/feedback';
 
 
 const app = express();
-app.use(express.static(path.join(__dirname , '../client/build')));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static(path.join(__dirname , '../client/build')));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 const port = process.env.PORT || 2000;
 
 
-
-app.get('/api/task', Tasks.index);
-app.get('/api/task/show', Tasks.show);
-app.post('/api/feedback', Feedbacks.create);
+//
+// app.get('/api/task', Tasks.index);
+// app.get('/api/task/show', Tasks.show);
+// app.post('/api/feedback', Feedbacks.create);
 
 
 app.get('/*', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.send('welcome');
+    //res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
