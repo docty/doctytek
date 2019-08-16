9551c2bd72e3d30cbb695dfdf728d6ddc7d147b6
docty <=>
2019-08-16 01:08:28 +0000

    maybe pg

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,5 @@
 
 require('dotenv').config(); // postgresql-octagonal-85731
-var database_url = process.env.DATABASE_URL;
 module.exports = {
 "development": {
     "username": process.env.DB_USERNAME,
@@ -17,11 +16,11 @@ module.exports = {
     "dialect": "mysql"
 },
 "production": {
-    "username": database_url['user'],
-    "password": database_url['pass'],
-    "database": database_url['path'],
-    "host": database_url['host'],
-    "port" : database_url['port'],
+    "use_env_variable" : "DATABASE_URL",
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
     "dialect": "postgres"
 }
 };
